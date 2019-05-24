1f259d6ccc7a6c35699eca1962d4093f40c1feae
John Webster <JohnLobster@comcast.net>
2019-05-23 20:35:57 -0700

    trying es5 output to debug

--- run.js
+++ run.js
@@ -1,4 +1,5 @@
 // node.js does not understand the es6 export/import
 // This hack allows imports and exports to work
 require = require("esm")(module);
+debugger;
 module.exports = require("./wordGameNode.js");
